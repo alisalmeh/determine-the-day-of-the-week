4ea8e80b45e24afb7442b5e4e6eaeac4206ff6f6
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-11 11:26:32 +0330

    add initial commit

---
+++ Days.cs
@@ -0,0 +1,13 @@
+namespace AliSalmeh_ProjectWeek4.WeekDays_Refactor
+{
+    public enum Days
+    {
+        Saturday = 1,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek4.WeekDays_Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day number (between 1-7): ");
+
+            var currentDay = (Days)Enum.Parse(typeof(Days), Console.ReadLine());
+            Console.WriteLine(currentDay);
+        }
+    }
+}
